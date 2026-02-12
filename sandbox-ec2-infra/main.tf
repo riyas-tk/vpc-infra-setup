8146ebbad111747b5c2a8b32df21efd4200d2694
Riyaz Tevar <riyaz_tk@yahoo.in>
2026-02-12 17:59:23 +0530

    fix key

--- sandbox-ec2-infra/main.tf
+++ sandbox-ec2-infra/main.tf
@@ -16,7 +16,8 @@ module "spot_instances" {
   instance_type   = each.value.type
   subnet_id       = module.my_vpc_infra.subnet_ids[each.value.subnet].id
   sg_id           = each.value.public_sg ? module.my_vpc_infra.public_sg_id : module.my_vpc_infra.private_sg_id
-  ssh_key         = aws_key_pair.ssh_key.key_pair_id
+  ssh_key         = aws_key_pair.ssh_key.id
   valid_until_hrs = var.valid_until_hrs
-
+  depends_on = [ aws_key_pair.ssh_key 
+  ]
 }
