5b8f56a767f054e1d1bd615e561362899f0d0641
Riyaz Tevar <riyaz_tk@yahoo.in>
2026-02-12 18:24:32 +0530

    update

--- sandbox-ec2-infra/output.tf
+++ sandbox-ec2-infra/output.tf
@@ -2,3 +2,7 @@
 output "instance_public_dns" {
   value = module.spot_instances["node_bastion"].ec2_public_dns
 }
+
+output "node1_ip" {
+  value = module.spot_instances["subnet-priv1"].ec2_private_dns
+}
